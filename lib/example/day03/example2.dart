52af600238d36a9321f1c59a4300796ca2d86a1a
itdanjalog <kgs2072@naver.com>
2025-11-05 12:02:19 +0900

    day03 DIO 예제

--- lib/example/day03/example2.dart
+++ lib/example/day03/example2.dart
@@ -12,6 +12,69 @@ class MyApp extends StatelessWidget{ // 상태(useState)가 없는 위젯
   }
 }
 // [3] 위젯 만들기2 < 페이지 > , 컴포넌트 역할
-class GoodsWidget extends StatefulWidget{ } // 상태(useSate) 가 있는 위젯
+class GoodsWidget extends StatefulWidget{  // 상태(useSate) 가 있는 위젯
+  // 상태 위젯 생성/등록
+  @override GoodsWidgetState createState() => GoodsWidgetState();
+}
+
+// [4] 상태 하위 위젯
+class GoodsWidgetState extends State<GoodsWidget>{
+  // [4-1] 입력 상자의 컨트롤러
+  TextEditingController nameCont = TextEditingController();
+  TextEditingController priceCont = TextEditingController();
+  TextEditingController descCont = TextEditingController();
+
+  // [4-2] goodsSave 함수정의
+  void goodsSave (){
+    final obj = {
+      'gname' : nameCont.text ,
+      'gprice' : priceCont.text ,
+      'gdesc' : descCont.text
+    }; print( obj );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( title: Text("제품 현황") ),
+      body: Padding( 
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+             TextField(
+               decoration: InputDecoration( labelText: "제품명") ,
+               controller: nameCont ,
+             ) ,
+             SizedBox( height: 20 ) ,
+
+             TextField(
+               decoration: InputDecoration( labelText: "제품가격") ,
+               keyboardType: TextInputType.number, // 모바일 환경에서 키보드 형식을 숫자패드
+               controller: priceCont,
+             ) ,
+             SizedBox( height: 20 ) ,
+
+             TextField(
+               decoration: InputDecoration( labelText: "제품설명"),
+               controller: descCont,
+             ) ,
+             SizedBox( height: 50 ) ,
+
+             ElevatedButton( onPressed: goodsSave , child: Text("제품등록") ) ,
+          ],
+        ),
+      ),
+    );
+  }
+} 
+// Padding : 안쪽 여백
+// Column( children : [ 위젯, 위젯 , 위젯 ] ) : 세로 정렬 ,
+
+
+
+
+
+
+
 
 
