c123a6d4494b197f3b7517138654c158ad6c7ae2
itdanjalog <kgs2072@naver.com>
2025-11-05 13:10:35 +0900

    day03 DIO 예제

--- lib/example/day03/example2.dart
+++ lib/example/day03/example2.dart
@@ -35,24 +35,21 @@ class GoodsWidgetState extends State<GoodsWidget>{
       // (web) 빌드시 : localhost:8080 가능 , (mobile) : 192.168.40.185:8080 만 가능
       // 스프링 컨트롤러 에서  @CrossOrigin(origins = "*") 또는 corsConfig
       final response = await dio.post("http://192.168.40.185:8080/api/goods" , data : obj );
-      final data = response.data;
-      print( data );
+      final data = response.data; print( data );
       // (1) 저장 성공된 자료를 추가하여 재렌더링 또는 (2) goodAll 재호출
       goodsAll(); // vs // setState(() { goodsList.add( data ); } );
     }catch(e){ print(e); }
   }
-
-  // [4-3] goodsAll 함수 정의 , useEffect( ()=>{} , [] ) 처럼 위젯이 최초로 열렸을때 1번실행
+  // [4-3] goodsAll 함수 정의
   void goodsAll() async{
     try{
       final dio = Dio();
       final response = await dio.get("http://192.168.40.185:8080/api/goods/list");
-      final data = response.data;
-      print( data ); // 통신 결과
+      final data = response.data; print( data ); // 통신 결과
       setState( () { goodsList = data;  } ); // 통신 결과를 함수 밖 변수(상태) 업데이트(렌더링)
     }catch(e){ print(e); }
   }
-  // [4-4]  initState() 함수 오버라이딩 하여 웨젯의 초기 상태
+  // [4-4]  initState() 함수 오버라이딩 하여 웨젯의 초기 상태 , useEffect( ()=>{} , [] ) 처럼 위젯이 최초로 열렸을때 1번실행
   @override // 오버라이딩
   void initState() { goodsAll(); } // 현재 위젯이 최초 실행될때 1번 goodAll 함수 실행
   // [4-5] dio로 가져온 데이터를 저장하는 상태 변수
@@ -86,7 +83,21 @@ class GoodsWidgetState extends State<GoodsWidget>{
              SizedBox( height: 50 ) ,
 
              ElevatedButton( onPressed: goodsSave , child: Text("제품등록") ) ,
-          ],
+
+            SizedBox( height: 50 ) ,
+
+            Expanded( child: ListView.builder(
+                itemCount: goodsList.length,    // 리스트의 총개수 정의
+                itemBuilder: (context , index ) {
+                  dynamic goods = goodsList[index]; // index번째 goods 1개 반환
+                  // print( goods );
+                  return Card( child : ListTile(
+                    title: Text( goods['gname'] ), // 객체명['속성명']
+                    subtitle: Text( goods['gdesc']),
+                  ) );
+                } )
+            )
+          ], // 세로 정렬 웨젯들
         ),
       ),
     );
