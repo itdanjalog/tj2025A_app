41742606422da890cd883fad0888316064f4d0d0
itdanjalog <kgs2072@naver.com>
2025-11-07 10:26:04 +0900

    day04

--- lib/example/day04/main.dart
+++ lib/example/day04/main.dart
@@ -7,7 +7,6 @@ void main(){ // 프로그램 실행 시작점
   runApp( MyApp() ); // runApp() 플러터 앱을 시작하는 함수
   // new MyApp() 내가 만든 루트(최초) 위젯 , new 생략가능
 }
-
 class MyApp extends StatelessWidget{ // 앱 전체의 루트 위젯 정의
   // StatelessWidget : 상태 없는 위젯으로 부터 상속받기
   @override
